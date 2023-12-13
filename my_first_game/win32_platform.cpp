dcd9663eb7790e3e2105f9e328ea3e646034e5f8
Kishan Chauhan <mtcup2000@yahoo.com>
2023-12-13 14:41:52 -0600

    Create function to draw graphics onto window

--- my_first_game/win32_platform.cpp
+++ my_first_game/win32_platform.cpp
@@ -1,6 +1,18 @@
 #include <windows.h>
+#include "utils.cpp"
 
-bool running = true;
+global_variable bool running = true;
+
+struct Render_State
+{
+	void* memory;
+	int width;
+	int height;
+	BITMAPINFO bitmap_info;
+};
+global_variable Render_State render_state;
+
+#include "renderer.cpp"
 
 //WNDCLASS structure documentation
 //https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-wndclassa
@@ -10,16 +22,35 @@ LRESULT CALLBACK window_callback(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lPar
 	LRESULT result = 0;
 	switch (Msg)
 	{
-		case WM_CLOSE:
-		case WM_DESTROY:
-		{
-			running = false;
-		} break;
+	case WM_CLOSE:
+	case WM_DESTROY:
+	{
+		running = false;
+	} break;
 
-		default:
-		{
-			result = DefWindowProc(hWnd, Msg, wParam, lParam);
-		}
+	case WM_SIZE: {
+		RECT rect;
+		GetClientRect(hWnd, &rect);
+		render_state.width = rect.right - rect.left;
+		render_state.height = rect.bottom - rect.top;
+
+		int size = render_state.width * render_state.height * sizeof(u32);
+
+		if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+		render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+		render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+		render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+		render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+		render_state.bitmap_info.bmiHeader.biPlanes = 1;
+		render_state.bitmap_info.bmiHeader.biBitCount = 32;
+		render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+	} break;
+
+	default:
+	{
+		result = DefWindowProc(hWnd, Msg, wParam, lParam);
+	}
 	}
 	return result;
 }
@@ -34,18 +65,31 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
 	//create register class
 	RegisterClass(&window_class);
-	
+
 	//https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowa
 	//create window
 	HWND window = CreateWindow(window_class.lpszClassName, L"My First Game!", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	HDC hdc = GetDC(window);
 
 	while (running)
 	{
+		//input
 		MSG message;
 		while (PeekMessage(&message, window, 0, 0, PM_REMOVE))
 		{
 			TranslateMessage(&message);
 			DispatchMessageW(&message);
 		}
+
+		//simulate
+		clear_screen(0xff5500);
+		draw_rect(0, 0, 1, 1, 0x00ff22);
+		draw_rect(30, 30, 5, 5, 0x00ff22);
+		draw_rect(-20, 20, 8, 3, 0x00ff22);
+
+		
+
+		//render
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 	}
 }
